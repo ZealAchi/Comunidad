d13aeba3ee92203f65ab73208eade25d43f1c47b
luispadre <luis.padre@protonmail.com>
2020-06-13 13:46:05 -0500

    1 file modified

--- src/Context/Notification.Context.jsx
+++ src/Context/Notification.Context.jsx
@@ -3,9 +3,9 @@ import React,{useState,createContext} from 'react'
 export const NotificationContext=createContext()
 const initialData={
   data:{
-    type:'Aviso',
-    owner:'Luis Antonio',
-    message:'Pago de Agua 25 Junio del 2020',
+    type:'Avisosad',
+    owner:'Luis Anasdtonio',
+    message:'Hola',
     view:false
   }
 }
